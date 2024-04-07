3b91edd1bc4669e662cd26877ba46c8b125f31ce
rmariuso <r_marius_o@yahoo.com>
2024-04-07 10:36:56 +0300

    changes

--- WiFiAP_HA/data/00-init.sh
+++ WiFiAP_HA/data/00-init.sh
@@ -5,8 +5,8 @@
 # ==============================================================================
 
 # Run dnsmasq
-bashio::log.info "Init..."  
-declare DIR 
+bashio::log.info "Init..."
+declare DIR
 declare DIR_RASPAP
 DIR="/config/wifiap_ha"
 DIR_RASPAP="${DIR}/raspap"
