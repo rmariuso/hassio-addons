b52a1c9935e1926d6781a098d634e2da9af01489
rmariuso <60664876+rmariuso@users.noreply.github.com>
2024-04-07 11:34:16 +0300

    Update run.sh

--- WiFiAP_HA/run.sh
+++ WiFiAP_HA/run.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/with-contenv bashio
-# ==============================================================================
-# Home Assistant Community Add-on: Pi-hole
-# Runs the hostapd daemon
-# ==============================================================================
 
 # Run dnsmasq
 #bashio::log.info "Init..."
 echo "Hello world!"
-declare DIR
-declare DIR_RASPAP
-DIR="/config/wifiap_ha"
-DIR_RASPAP="${DIR}/raspap"
 #LANG="$(bashio::config 'language')"
