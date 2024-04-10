b4e2ad2ff65b896ecfa53cb85bf86ea2bab1bba4
rmariuso <60664876+rmariuso@users.noreply.github.com>
2024-04-11 00:15:02 +0300

    changes

--- WiFi_AP/rootfs/etc/config.php
+++ WiFi_AP/rootfs/etc/config.php
@@ -1,7 +1,7 @@
 <?php
 
 define('RASPI_BRAND_TEXT', 'wifiAP');
-define('HA_CONFIG', '/config/wifiap_ha_last');
+define('HA_CONFIG', '/config/wifiap');
 define('RASPI_CONFIG', HA_CONFIG.'/raspap');
 define('RASPI_CONFIG_NETWORK', RASPI_CONFIG.'/networking/defaults.json');
 define('RASPI_CONFIG_PROVIDERS', 'config/vpn-providers.json');
