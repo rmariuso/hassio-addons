b279250f82d265c62c6d03b06dd0cf3d4cf0b762
rmariuso <60664876+rmariuso@users.noreply.github.com>
2024-04-21 15:57:26 +0300

    changes

--- wifiap/run.sh
+++ wifiap/run.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Start a long-running process to keep the container pipes open
-sleep infinity < /proc/1/fd/0 > /proc/1/fd/1 2>&1 &
+#sleep infinity < /proc/1/fd/0 > /proc/1/fd/1 2>&1 &
 # Wait a bit before retrieving the PID
-sleep 1
+#sleep 1
 # Save the long-running PID on file
-echo $! > /container-pipes-pid
+#echo $! > /container-pipes-pid
 # Start systemd as PID 1
-echo "Hello world!"
-exec /usr/lib/systemd/systemd
-
 #echo "Hello world!"
-#python3 -m http.server 8080
+#exec /usr/lib/systemd/systemd
+
+echo "Hello world!"
+python3 -m http.server 8080
 
 #/etc/init.d/dhcpcd start && /etc/init.d/dnsmasq start && /etc/init.d/hostapd start && /etc/init.d/lighttpd start
