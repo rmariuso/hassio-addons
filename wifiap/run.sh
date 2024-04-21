5ee1f353dd19f55a426ebf584ce8f4f6114dc655
rmariuso <60664876+rmariuso@users.noreply.github.com>
2024-04-21 11:45:07 +0300

    change

--- wifiap/run.sh
+++ wifiap/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#echo "Hello world!"
-#python3 -m http.server 8080
+echo "Hello world!"
+python3 -m http.server 8080
 
-/etc/init.d/dhcpcd start && /etc/init.d/dnsmasq start && /etc/init.d/hostapd start && /etc/init.d/lighttpd start
+#/etc/init.d/dhcpcd start && /etc/init.d/dnsmasq start && /etc/init.d/hostapd start && /etc/init.d/lighttpd start
