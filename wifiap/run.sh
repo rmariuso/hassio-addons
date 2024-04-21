0f99188f21578d1f543215cbee79f9ff608c4ab8
rmariuso <60664876+rmariuso@users.noreply.github.com>
2024-04-22 00:55:10 +0300

    change

--- wifiap/run.sh
+++ wifiap/run.sh
@@ -55,4 +55,4 @@ fi
 /etc/init.d/dhcpcd start
 /etc/init.d/dnsmasq start
 sleep 4
-/etc/init.d/lighttpd start && vnstatd -d --config "${DIR}/raspap/vnstat.conf" && hostapd ${DIR}/hostapd/hostapd.conf 1> /dev/null
+/etc/init.d/lighttpd start && vnstatd --config "${DIR}/raspap/vnstat.conf" && hostapd ${DIR}/hostapd/hostapd.conf 1> /dev/null
