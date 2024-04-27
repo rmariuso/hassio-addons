07e2d35bd66299a31e52d45b83a075bc4c10c7e4
rmariuso <60664876+rmariuso@users.noreply.github.com>
2024-04-27 09:38:46 +0300

    change

--- WiFi_AP/run.sh
+++ WiFi_AP/run.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-sudo chown -R www-data:www-data /var/www/html
-sudo chown -R www-data:www-data /config/wifiap
-sudo mv /var/www/html/installers/enablelog.sh /config/wifiap/hostapd
-sudo mv /var/www/html/installers/disablelog.sh /config/wifiap/hostapd
-sudo mv /var/www/html/installers/servicestart.sh /config/wifiap/hostapd
-sudo mv /var/www/html/installers/debuglog.sh /config/wifiap/system
-sudo chown -c root:root /config/wifiap/hostapd/*.sh
-sudo chmod 750 /config/wifiap/hostapd/*.sh
-sudo chown -c root:root /config/wifiap/system/*.sh
-sudo chmod 750 /config/wifiap/system/*.sh
-sudo cp /var/www/html/installers/configport.sh /config/wifiap/lighttpd
-sudo chown -c root:root /config/wifiap/lighttpd/*.sh
-sudo mv /etc/default/hostapd ~/default_hostapd.old
-sudo cp /etc/hostapd/hostapd.conf ~/hostapd.conf.old
-sudo cp /var/www/html/config/hostapd.conf /config/wifiap/hostapd/hostapd.conf
-sudo cp /var/www/html/config/dhcpcd.conf /config/wifiap/dhcpcd/dhcpcd.conf
-sudo cp /var/www/html/config/defaults.json /config/wifiap/networking/
-sudo cp /var/www/html/config/raspap-bridge-br0.netdev /etc/systemd/network/raspap-bridge-br0.netdev
-sudo cp /var/www/html/config/raspap-br0-member-eth0.network /etc/systemd/network/raspap-br0-member-eth0.network
+chown -R www-data:www-data /var/www/html
+chown -R www-data:www-data /config/wifiap
+mv /var/www/html/installers/enablelog.sh /config/wifiap/hostapd
+mv /var/www/html/installers/disablelog.sh /config/wifiap/hostapd
+mv /var/www/html/installers/servicestart.sh /config/wifiap/hostapd
+mv /var/www/html/installers/debuglog.sh /config/wifiap/system
+chown -c root:root /config/wifiap/hostapd/*.sh
+chmod 750 /config/wifiap/hostapd/*.sh
+chown -c root:root /config/wifiap/system/*.sh
+chmod 750 /config/wifiap/system/*.sh
+cp /var/www/html/installers/configport.sh /config/wifiap/lighttpd
+chown -c root:root /config/wifiap/lighttpd/*.sh
+mv /etc/default/hostapd ~/default_hostapd.old
+cp /etc/hostapd/hostapd.conf ~/hostapd.conf.old
+cp /var/www/html/config/hostapd.conf /config/wifiap/hostapd/hostapd.conf
+cp /var/www/html/config/dhcpcd.conf /config/wifiap/dhcpcd/dhcpcd.conf
+cp /var/www/html/config/defaults.json /config/wifiap/networking/
+cp /var/www/html/config/raspap-bridge-br0.netdev /etc/systemd/network/raspap-bridge-br0.netdev
+cp /var/www/html/config/raspap-br0-member-eth0.network /etc/systemd/network/raspap-br0-member-eth0.network
 
 
 echo "Hello world!"
