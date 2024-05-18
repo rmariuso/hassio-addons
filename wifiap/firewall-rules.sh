ce41c9f0e0544f514ee4d139553628c1b513ff58
rmariuso <60664876+rmariuso@users.noreply.github.com>
2024-05-18 15:00:25 +0300

    changes wifiap

--- wifiap/firewall-rules.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#iptables -I DOCKER-USER -i src_if -o dst_if -j ACCEPT
-#iptables -t nat -C POSTROUTING -o eth0 -j MASQUERADE || iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-#iptables -C FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT || iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-#iptables -C FORWARD -i wlan0 -o eth0 -j ACCEPT || iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
-#iptables-save
-iptables-nft -t nat -A POSTROUTING -o end0 -j MASQUERADE
-iptables-nft -P FORWARD ACCEPT
-iptables-nft -F FORWARD
